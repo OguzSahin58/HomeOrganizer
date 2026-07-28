957a470e971f9da0c61df8a2a38ef1060b54ff50
oguzsahin58 <oguz.sahin.20742@ozu.edu.tr>
2026-07-28 19:07:17 +0300

    Implement ItemsController with CRUD operations for items

---
+++ HomeOrganizer.Api/Controllers/ItemsController.cs
@@ -0,0 +1,45 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace HomeOrganizer.Api
+{
+    [Route("items")]
+    [ApiController]
+    public class ItemsController : ControllerBase
+    {
+        private static readonly List<ItemDto> Items = new()
+        {
+            new ItemDto(1, "Bavul", "Item container", DateOnly.FromDateTime(DateTime.Now)),
+            new ItemDto(2, "Canta", "Item for carrying belongings", DateOnly.FromDateTime(DateTime.Now)),
+            new ItemDto(3, "Ayakkabi", "Footwear", DateOnly.FromDateTime(DateTime.Now))
+        };
+
+        [HttpGet]
+        public ActionResult<IEnumerable<ItemDto>> GetItems()
+        {
+            return Ok(Items);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<ItemDto> GetItem(int id)
+        {
+            var item = Items.Find(i => i.Id == id);
+
+            return item is not null ? Ok(item) : NotFound();
+        }
+
+        [HttpPost]
+        public ActionResult<ItemDto> CreateItem(CreateItemDto newItem)
+        {
+            ItemDto item = new(
+                Items.Count + 1,
+                newItem.Name,
+                newItem.Description,
+                newItem.LastModifiedDate
+            );
+
+            Items.Add(item);
+
+            return Created($"/items/{item.Id}", item);
+        }
+    }
+}
