d4b3b81a0bea959aec54a21eeb1f0b4ba954b4e3
oguzsahin58 <oguz.sahin.20742@ozu.edu.tr>
2026-07-31 15:45:03 +0300

    EF Core setup with Home entity and ApplicationDbContext

---
+++ HomeOrganizer.Api/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+using HomeOrganizer.Api.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace HomeOrganizer.Api.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Home> Homes => Set<Home>();
+}
---
+++ HomeOrganizer.Api/Entities/Home.cs
@@ -0,0 +1,8 @@
+namespace HomeOrganizer.Api.Entities;
+
+public class Home
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = string.Empty;
+}
